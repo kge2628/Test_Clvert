23d3619c46617c4108d430fa1db6669c18d79b63
kge2628 <kge2628@gmail.com>
2026-02-10 08:37:39 +0000

    Initial commit

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-// Add JS here
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+});
